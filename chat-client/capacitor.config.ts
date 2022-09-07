758969339004139c77d07cdd9c01f30324bd8296
Lê Huỳnh Đức <duclh@MacBook-Pro-cua-Le.local>
2022-09-07 23:52:03 +0700

    add ionic

---
+++ chat-client/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'chat.client',
+  appName: 'chat-client',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
