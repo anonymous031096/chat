5159ff1778e742a795570bfd6e190fc7582a2c66
Lê Huỳnh Đức <duclh@MacBook-Pro-cua-Le.local>
2022-09-08 18:52:44 +0700

    u

---
+++ chat-client/src/app/app.routes.ts
@@ -0,0 +1,12 @@
+
+
+import { Routes } from '@angular/router';
+// import { PAGE_ROUTES } from './routes/page.routes';
+
+export const APP_ROUTES: Routes = [
+//   {
+//     path: '',
+//     loadComponent: () => import('./wrapper/wrapper.component').then((c) => c.WrapperComponent),
+//     children: PAGE_ROUTES,
+//   },
+];
--- chat-client/src/main.ts
+++ chat-client/src/main.ts
@@ -1,12 +1,15 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { enableProdMode, importProvidersFrom } from '@angular/core'
+import { bootstrapApplication, BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { RouterModule } from '@angular/router'
+import { AppComponent } from './app/app.component'
+import { APP_ROUTES } from './app/app.routes'
+import { environment } from './environments/environment'
 
 if (environment.production) {
-  enableProdMode();
+  enableProdMode()
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(RouterModule.forRoot(APP_ROUTES), BrowserModule, BrowserAnimationsModule)],
+})
