375931a7fd8ce2bb867454ae918b4ea5beeebd96
Lê Huỳnh Đức <duclh@MacBook-Pro-cua-Le.local>
2022-09-09 22:32:28 +0700

    app bar

---
+++ fchat/fchat/lib/screens/home_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 4, vsync: this, initialIndex: 0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Whatsapp"),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert))
+        ],
+        bottom: TabBar(controller: _tabController, tabs: const [
+          Tab(
+            icon: Icon(Icons.camera_alt),
+          ),
+          Tab(
+            text: "CHATS",
+          ),
+          Tab(
+            text: "STATUS",
+          ),
+          Tab(
+            text: "CALLS",
+          ),
+        ]),
+      ),
+      body: TabBarView(controller: _tabController, children: const [
+        Text("camera"),
+        Text("chats"),
+        Text("status"),
+        Text("calls"),
+      ]),
+    );
+  }
+}
