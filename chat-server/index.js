76c960c63836425c70d015fc627a2eafdb5871e2
Lê Huỳnh Đức <duclh@MacBook-Pro-cua-Le.local>
2022-09-11 13:58:05 +0700

    server chat

---
+++ chat-server/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+var http = require("http");
+const app = express();
+const port = process.env.PORT || 5000;
+var server = http.createServer(app);
+var io = require("socket.io")(server);
+
+//middlewre
+app.use(express.json());
+var clients = {};
+
+io.on("connection", (socket) => {
+  socket.on("signin", (id) => {
+    console.log(id);
+    clients[id] = socket;
+  });
+  socket.on("message", (msg) => {
+    console.log(msg);
+    let targetId = msg.targetId;
+    if (clients[targetId]) clients[targetId].emit("message", msg);
+  });
+});
+
+server.listen(port, "0.0.0.0", () => {
+  console.log("server started");
+});
